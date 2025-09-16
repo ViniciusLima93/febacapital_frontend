120c700eef41d02325cd04e6c092862717ae5c63
ViniciusLima93 <vnlima9@gmail.com>
2025-09-16 18:18:21 -0400

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {  Urbanist} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const urbanistSans = Urbanist({
+  variable: "--font-urbanist",
+  subsets: ["latin"]
+})
 
 export const metadata: Metadata = {
   title: "LIVA_TEST-Técnico",
@@ -23,9 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${urbanistSans.variable} font-sans antialiased`}
       >
         {children}
       </body>
